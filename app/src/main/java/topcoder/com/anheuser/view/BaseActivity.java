31917fc17fda33bef6e8142f72120254a8be5c31
Ahmad Fadli Basyari <abasyari@traveloka.com>
2015-12-27 03:29:09 +0700

    Adding dependencies

---
+++ app/src/main/java/topcoder/com/anheuser/view/BaseActivity.java
@@ -0,0 +1,9 @@
+package topcoder.com.anheuser.view;
+
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by ahmadfadli on 12/27/15.
+ */
+public class BaseActivity extends AppCompatActivity {
+}
